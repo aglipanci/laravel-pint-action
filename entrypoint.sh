f8d8a4fbd5fc81f0064ce14b0166f906356582e2
Agli Pançi <aglipanci@users.noreply.github.com>
2022-12-21 23:44:44 +0100

    dynamic pint version

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 set -e
 
+pint_install_command=("composer global require laravel/pint:PINT_VERSION --no-progress --dev")
+
+if [[ "${INPUT_PINT_VERSION}" ]]
+then
+ pint_install_command="${pint_install_command/PINT_VERSION/${INPUT_PINT_VERSION}}"
+else
+ pint_install_command="${pint_install_command/:PINT_VERSION/}"
+fi
+
 command_string=("pint")
 
 if [[ "${INPUT_TESTMODE}" ]]; then
@@ -19,7 +28,7 @@ if [[ "${INPUT_PRESET}" ]]; then
   command_string+=" --preset ${INPUT_PRESET}"
 fi
 
-composer global require laravel/pint --no-progress --dev
+${pint_install_command[@]}
 PATH="/tmp/vendor/bin:${PATH}"
 
 echo "Running Command: " "${command_string[@]}"
